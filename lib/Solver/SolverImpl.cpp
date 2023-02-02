5911d65be2a80e67f03e3ab8afde6b846c460243
Sergey Morozov <morozov.serg901@gmail.com>
2023-02-02 14:40:27 +0300

    Slightly optimized minimiztion function: now to find right border we can make a binary search

--- lib/Solver/SolverImpl.cpp
+++ lib/Solver/SolverImpl.cpp
@@ -74,26 +74,81 @@ bool SolverImpl::computeMinimalUnsignedValue(const Query &query,
   ref<ConstantExpr> left = ConstantExpr::create(0, query.expr->getWidth());
   ref<ConstantExpr> right = ConstantExpr::create(1, query.expr->getWidth());
 
-  /* It is a good strategy to find the appropriate bounds to start.
-  We will waste 2 queries here to possibly approximate borders.
-  (This might be useful if we have constraints like query.expr > 100'000) */
-  // TODO:
+  /* It is a good idea to find the appropriate bounds to start.
+  To do this we need to find x, such that if 'expr > 2**x', then
+  number of queries will be around x+-1 to find the upperbound. So if
+  we solve 'log log(2**base - 2**x) == x', we will find optimal 'x'. */
 
-  // Compute the right border
-  bool firstIteration = true;
-  do {
-    if (!firstIteration) {
-      left = right;
-      right = cast<ConstantExpr>(
-          ShlExpr::create(right, ConstantExpr::create(1, right->getWidth())));
+  /* In other words we solve 2**base = 2**x + 2**(2**x). As function
+  in the right part grows monotonously, we can use binary search
+  to approximate solution, or just pick x = log_2(base) 
+  as 2**x == o(2**(2**x)). */
+  
+  assert(query.expr->getWidth() > 0);
+  bool isHighRightBound = false;
+  int rightmostWidthBit = sizeof(unsigned long long) * CHAR_BIT - 1 -
+                          __builtin_clzll(query.expr->getWidth());
+  ref<Expr> inequalityValueOptimizatonExpr = ShlExpr::create(
+      ConstantExpr::create(1, query.expr->getWidth()),
+      ConstantExpr::create(rightmostWidthBit, query.expr->getWidth()));
+
+  if (!computeTruth(query.withExpr(
+                        UgtExpr::create(query.expr, inequalityValueOptimizatonExpr)),
+                    isHighRightBound)) {
+    return false;
+  }
+
+  if (isHighRightBound) {
+    left = ConstantExpr::create(rightmostWidthBit, query.expr->getWidth());
+    right =
+        ConstantExpr::create(query.expr->getWidth(), query.expr->getWidth());
+    /* while (left + 1 < right) */
+    while (cast<ConstantExpr>(
+               UltExpr::create(AddExpr::create(left, ConstantExpr::create(
+                                                         1, left->getWidth())),
+                               right))
+               ->isTrue()) {
+      ref<ConstantExpr> middle =
+          LShrExpr::create(AddExpr::create(left, right),
+                           ConstantExpr::create(1, left->getWidth()));
+      ref<Expr> valueMustBeGreaterExpr = ConstraintManager::simplifyExpr(
+          query.constraints,
+          UgtExpr::create(
+              query.expr,
+              ShlExpr::create(ConstantExpr::create(1, middle->getWidth()),
+                              middle)));
+
+      bool mustBeGreater = false;
+      if (!computeTruth(query.withExpr(valueMustBeGreaterExpr),
+                        mustBeGreater)) {
+        return false;
+      }
+
+      if (mustBeGreater) {
+        left = middle;
+      } else {
+        right = middle;
+      }
     }
-    ref<Expr> valueMustBeGreaterExpr = ConstraintManager::simplifyExpr(
-        query.constraints, UgtExpr::create(query.expr, right));
-    if (!computeTruth(query.withExpr(valueMustBeGreaterExpr), mustBeTrue)) {
-      return false;
-    }
-    firstIteration = false;
-  } while (mustBeTrue);
+    left = ShlExpr::create(ConstantExpr::create(1, left->getWidth()), left);
+    right = ShlExpr::create(ConstantExpr::create(1, right->getWidth()), right);
+  } else {
+    // Compute the right border
+    bool firstIteration = true;
+    do {
+      if (!firstIteration) {
+        left = right;
+        right = cast<ConstantExpr>(
+            ShlExpr::create(right, ConstantExpr::create(1, right->getWidth())));
+      }
+      ref<Expr> valueMustBeGreaterExpr = ConstraintManager::simplifyExpr(
+          query.constraints, UgtExpr::create(query.expr, right));
+      if (!computeTruth(query.withExpr(valueMustBeGreaterExpr), mustBeTrue)) {
+        return false;
+      }
+      firstIteration = false;
+    } while (mustBeTrue);
+  }
 
   // Binary search the least value for expr from the given query
   while (cast<ConstantExpr>(
@@ -101,20 +156,20 @@ bool SolverImpl::computeMinimalUnsignedValue(const Query &query,
                                                        1, left->getWidth())),
                              right))
              ->isTrue()) {
-    ref<ConstantExpr> mid = cast<ConstantExpr>(
+    ref<ConstantExpr> middle = cast<ConstantExpr>(
         LShrExpr::create(AddExpr::create(left, right),
                          ConstantExpr::create(1, right->getWidth())));
     ref<Expr> valueMustBeGreaterExpr = ConstraintManager::simplifyExpr(
-        query.constraints, UgtExpr::create(query.expr, mid));
+        query.constraints, UgtExpr::create(query.expr, middle));
 
     if (!computeTruth(query.withExpr(valueMustBeGreaterExpr), mustBeTrue)) {
       return false;
     }
 
     if (mustBeTrue) {
-      left = mid;
+      left = middle;
     } else {
-      right = mid;
+      right = middle;
     }
   }
 
