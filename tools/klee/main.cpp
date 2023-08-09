c4b63fd2b5983e7817004cb58494c8907ebd958e
Sergey Morozov <morozov.serg901@gmail.com>
2023-08-09 12:40:18 +0300

    Fixed restrictions on test case generation for testcomp

--- tools/klee/main.cpp
+++ tools/klee/main.cpp
@@ -570,7 +570,8 @@ void KleeHandler::processTestCase(const ExecutionState &state,
                                   const char *message, const char *suffix,
                                   bool isError) {
   unsigned id = ++m_numTotalTests;
-  if (!WriteNone && (!CoverageErrorCall || strcmp(suffix, ".assert.err") != 0)) {
+  if (!WriteNone && (!CoverageErrorCall || strcmp(suffix, "assert.err") == 0 ||
+                     strcmp(suffix, "reachable.err") == 0)) {
     KTest ktest;
     ktest.numArgs = m_argc;
     ktest.args = m_argv;
