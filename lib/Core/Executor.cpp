11751fb9464d67e95c89304c72282d9a2c375e7c
Sergey Morozov <morozov.serg901@gmail.com>
2023-08-18 19:23:04 +0300

    Created shadow copy of errno in 32bit space to work with 32bit programs.

--- lib/Core/Executor.cpp
+++ lib/Core/Executor.cpp
@@ -732,14 +732,35 @@ void Executor::allocateGlobalObjects(ExecutionState &state) {
   }
 
 #ifndef WINDOWS
-  int *errno_addr = getErrnoLocation(state);
+
   llvm::Type *pointerErrnoAddr = llvm::PointerType::get(
       llvm::IntegerType::get(m->getContext(), sizeof(*errno_addr) * CHAR_BIT),
       adressSpaceNum);
-  MemoryObject *errnoObj =
-      addExternalObject(state, (void *)errno_addr,
-                        typeSystemManager->getWrappedType(pointerErrnoAddr),
-                        sizeof *errno_addr, false);
+  MemoryObject *errnoObj = nullptr;
+
+  if (Context::get().getPointerWidth() == 32) {
+    // This is not working (?) 
+    // errno_addr = (int *)mmap(NULL, sizeof(*errno_addr), PROT_READ | PROT_WRITE,
+    //                          MAP_32BIT | MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
+    errnoObj = allocate(state, Expr::createPointer(sizeof(*errno_addr)), false, true,
+             nullptr, 8);
+    errnoObj->isFixed = true;
+    
+    ObjectState *os = bindObjectInState(
+        state, errnoObj, typeSystemManager->getWrappedType(pointerErrnoAddr),
+        false);
+    errno_addr = reinterpret_cast<int *>(errnoObj->address);
+    for (unsigned i = 0; i < sizeof(*errno_addr); i++) {
+      os->write8(i, ((uint8_t *)errno_addr)[i]);
+    }
+  } else {
+    errno_addr = getErrnoLocation(state);
+    errnoObj =
+        addExternalObject(state, (void *)errno_addr,
+                          typeSystemManager->getWrappedType(pointerErrnoAddr),
+                          sizeof *errno_addr, false);
+  }
+
   // Copy values from and to program space explicitly
   errnoObj->isUserSpecified = true;
 #endif
@@ -4891,7 +4912,6 @@ void Executor::callExternalFunction(ExecutionState &state, KInstruction *target,
   state.addressSpace.copyOutConcretes();
 #ifndef WINDOWS
   // Update external errno state with local state value
-  int *errno_addr = getErrnoLocation(state);
   IDType idResult;
 
   llvm::Type *pointerErrnoAddr = llvm::PointerType::get(
