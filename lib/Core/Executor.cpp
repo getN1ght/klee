d2df5b2b602fe845189152e9dabd6d284205bec4
Sergey Morozov <morozov.serg901@gmail.com>
2024-03-21 14:01:47 +0100

    [refactor] Multiple fixes for compilation warnings.

--- lib/Core/Executor.cpp
+++ lib/Core/Executor.cpp
@@ -509,9 +509,9 @@ Executor::Executor(LLVMContext &ctx, const InterpreterOptions &opts,
                    InterpreterHandler *ih)
     : Interpreter(opts), interpreterHandler(ih), searcher(nullptr),
       externalDispatcher(new ExternalDispatcher(ctx)), statsTracker(0),
-      pathWriter(0), symPathWriter(0),
-      specialFunctionHandler(0), timers{time::Span(TimerInterval)},
-      guidanceKind(opts.Guidance), codeGraphInfo(new CodeGraphInfo()),
+      pathWriter(0), symPathWriter(0), specialFunctionHandler(0),
+      timers{time::Span(TimerInterval)}, guidanceKind(opts.Guidance),
+      codeGraphInfo(new CodeGraphInfo()),
       distanceCalculator(new DistanceCalculator(*codeGraphInfo)),
       targetCalculator(new TargetCalculator(*codeGraphInfo)),
       targetManager(new TargetManager(guidanceKind, *distanceCalculator,
@@ -5664,7 +5664,7 @@ bool Executor::resolveExact(ExecutionState &estate, ref<Expr> address,
   bool incomplete = false;
 
   /* We do not need this variable here, just a placeholder for resolve */
-  bool success = resolveMemoryObjects(
+  [[maybe_unused]] bool success = resolveMemoryObjects(
       state, pointer, type, state.prevPC, 0, rl, mayBeOutOfBound,
       hasLazyInitialized, incomplete,
       LazyInitialization == LazyInitializationPolicy::Only);
@@ -7289,8 +7289,7 @@ void Executor::logState(const ExecutionState &state, int id,
     object.first->getSizeExpr()->print(*f);
     *f << "\n";
   }
-  *f << state.symbolics.size() << " symbolics total. "
-     << "Symbolics:\n";
+  *f << state.symbolics.size() << " symbolics total. " << "Symbolics:\n";
   size_t sc = 0;
   for (const auto &symbolic : state.symbolics) {
     *f << "Symbolic number " << sc++ << "\n";
